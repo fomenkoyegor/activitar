eef4ac5fd793c0c5f20068fcebb810a00a98b608
fomenkoyegor <fomenkoyegor@gmail.com>
2022-04-19 11:06:34 +0300

    init next tailwind react-icons redux-toolkit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { FaBeer, FaShoppingCart } from 'react-icons/fa';
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +17,9 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h1 className="text-3xl font-bold underline">
+          <FaBeer/>Hello world!!!!!!<FaShoppingCart/>
+         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
