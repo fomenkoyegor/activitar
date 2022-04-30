2e5ceaff3cbb930f6484aa9e62c62f781e123411
fomenkoyegor <fomenkoyegor@gmail.com>
2022-04-30 10:38:15 +0300

    init prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
 import type { NextPage } from "next";
 import Layout from "../app/layouts";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
   return (
     <Layout>
       <h1>hello</h1>
+      <pre>{JSON.stringify(props.feed, null, 2)}</pre>
     </Layout>
   );
 };
 
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch('http://localhost:3000/api/feed')
+  const feed = await res.json() 
+  return {
+    props: { feed },
+  }
+}
 
 export default HomePage;
